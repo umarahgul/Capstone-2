95bd799d1dbd1a4495d66dae29207c801d033472
umarahgul <umarah.gul@gmail.com>
2023-09-13 23:42:12 +0500

    Added the popup on clicking comments button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import _ from 'lodash';
 import './Style.css';
 import logo from './assets/logo.jpg'
-import { populate } from './modules/populate';
-import heart from './assets/heart.png';
-
+import {render} from './modules/populate';
+import getCommentsCount from './modules/comments-counter';
+import {renderpopup} from './modules/popup';
 
 // document.addEventListener('DOMContentLoaded', () => {
 //   // This code will run after the DOM is fully loaded
@@ -12,7 +12,45 @@ import heart from './assets/heart.png';
 // });
 
 
-document.addEventListener('DOMContentLoaded',populate);
+// document.addEventListener('DOMContentLoaded', render);
 
-document.getElementById('logo-img').setAttribute('src', logo);
- await render();
+// document.getElementById('logo-img').setAttribute('src', logo);
+//  //render();
+
+// //..............  comment popup start ...............//
+
+//  const commentBtn = document.querySelectorAll('.commentBtn');
+
+//  // Add event to comment button
+//  commentBtn.forEach((btn) => {
+//    btn.addEventListener('click', async () => {
+//     alert ('i was clicked yo giffy');
+//      const { id } = btn.parentElement.parentElement;
+//      await renderpopup(id);
+ 
+ 
+//      const commentsCountCon = document.querySelector('.commentsCount');
+ 
+//      commentsCountCon.textContent = `(${getCommentsCount()})`;
+//    });
+//  });
+ //..........comment popup ends............//
+
+ document.addEventListener('DOMContentLoaded', () => {
+    // This code will run after the DOM is fully loaded
+    document.getElementById('logo-img').setAttribute('src', logo);
+    render(); // Call the render function after setting the logo src
+  
+    // Add a click event listener to a parent element that contains the .commentBtn buttons
+    document.body.addEventListener('click', (event) => {
+      if (event.target.classList.contains('commentBtn')) {
+        const btn = event.target;
+  //      alert('Button clicked: ' + btn.textContent); // Debugging alert
+        const { id } = btn.parentElement.parentElement;
+        renderpopup(id);
+       // const commentsCountCon = document.querySelector('.commentsCount');
+        //commentsCountCon.textContent = `(${getCommentsCount()})`;
+      }
+    });
+  });
+  
--- src/modules/populate.js
+++ src/modules/populate.js
@@ -1,34 +1,77 @@
-import heart from '../assets/heart.png';
-export const populate = () => {
 
-  const parent=document.getElementById('movies');
-  const movieArr=[" yum","yo", "yi"];
+const url ='https://api.tvmaze.com/shows';
 
-  for (let x=0; x < movieArr.length; x += 1)
-  {
-  const card=document.createElement('div');
-  card.setAttribute('id', 'card'+ `${x}`);
-  const movieImg=document.createElement('img');
- 
-  movieImg.style.width= '200px';
-  movieImg.style.height= '200px';
+// fetch datd from api
 
-  const likeButton=document.createElement('img');
-//   likeButton.src='./assets/heart.png';
-  likeButton.src=heart;
-  
-  const comment=document.createElement('button');
-  comment.value='Comments';
-  comment.classList.add('comment');
-  const reservation=document.createElement('button');
-  reservation.value='Reservations';
-  reservation.classList.add('reservation');
-  card.appendChild(movieImg);
-  card.appendChild(likeButton);
-  card.appendChild(comment);
-  card.appendChild(reservation);
-  
-  parent.appendChild(card);
+
+const getData = async  () => {
+  try {
+   
+    const response = await fetch(url); // Fetch data from api endpoints
+    const data = await response.json(); // Change the data format to json
+
+    return data;
+  } catch (error) {
+    return error.message;
   }
+};
 
+export const render = async () => {
+  const data = await getData();
+  const topThree = data
+    .sort((a, b) => b.rating.average - a.rating.average)
+    .slice(0, 3);
+
+    
+  
+    const tvShowsDiv = document.getElementById('movies');
+  
+  //const likes = await retrieveLikes();
+
+  for (let x=0; x <topThree.length; x += 1)
+  { 
+    alert (topThree[x].name);
+    const poster = topThree[x].image.medium;
+    const title =topThree[x].name;
+    const id=topThree[x].id;
+    
+    // a div containing image button and title
+    const showDiv = document.createElement('div');
+    showDiv.id = `${id}`;
+    showDiv.classList.add('series');
+
+    
+    const posterImg = document.createElement('img');
+    posterImg.src = poster;
+    //const icons = document.createElement('span');
+ 
+    // a container containing title
+    const titleContainer = document.createElement('div');
+    const showName=document.createElement('h2');
+    const likes=document.createElement('i');
+    likes.classList.add('fa-solid', 'fa-heart', 'likeBtn');
+    showName.textContent=title;
+     titleContainer.appendChild(showName);
+     titleContainer.appendChild(likes);
+     titleContainer.classList.add('div-title');
+
+
+    // a container containing comments button
+    const buttonContainer=document.createElement('div');
+    const commentBtn =document.createElement('button');
+    commentBtn.setAttribute('id',`cmmnt + ${x}`);
+    commentBtn.textContent='Comments';
+    commentBtn.classList.add('commentBtn');
+    buttonContainer.appendChild(commentBtn);
+    
+    
+    
+    showDiv.appendChild(posterImg);
+    showDiv.appendChild(titleContainer);
+    showDiv.appendChild(buttonContainer);
+    tvShowsDiv.appendChild(showDiv);
+  }
+  
 }
+
+
