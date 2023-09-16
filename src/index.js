43a171af866d77783905bccaf0744b06b3a4333d
umarahgul <umarah.gul@gmail.com>
2023-09-16 23:00:07 +0500

    removed linter errors

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-import './Style.css';
-import logo from './assets/logo.jpg';
-import { render } from './modules/populate';
-import { renderpopup } from './modules/popup';
-import { countLikes } from './modules/likes';
-import itemsCounter from './modules/itemsCounter';
+import "./Style.css";
+import logo from "./assets/logo.jpg";
+import { render } from "./modules/populate";
+import { renderpopup } from "./modules/popup";
+import { countLikes } from "./modules/likes";
+import itemsCounter from "./modules/itemsCounter";
 
 // document.addEventListener('DOMContentLoaded', () => {
 // This code will run after the DOM is fully loaded
 //import getCommentsCount from './modules/comments-counter';
-document.getElementById('logo-img').setAttribute('src', logo);
+
+document.getElementById("logo-img").setAttribute("src", logo);
 render(); // Call the render function after setting the logo src
 
-const heartIcons = document.querySelectorAll('.fa-heart');
-const spanItems = document.querySelectorAll('.itemCount');
+const heartIcons = document.querySelectorAll(".fa-heart");
+const spanItems = document.querySelectorAll(".itemCount");
 // Add the number of items in the span.
 spanItems.forEach((span) => {
   span.textContent = ` (${itemsCounter()})`;
 });
 
-heartIcons.forEach((icon) => icon.addEventListener('click', (event) => {
-  alert('likes ');
-  countLikes(event);
-  event.target.classList.add('animation-heart');
-  // Selects the span element next to the icon.
-  const span = event.target.previousElementSibling;
-  // Retrieves the number of likes and increment it by 1.
-  let count = Number(span.textContent.replace('likes', ''));
-  span.textContent = `${count += 1} likes`;
-}));
+heartIcons.forEach((icon) =>
+  icon.addEventListener("click", (event) => {
+    alert("likes ");
+    countLikes(event);
+    event.target.classList.add("animation-heart");
+    // Selects the span element next to the icon.
+    const span = event.target.previousElementSibling;
+    // Retrieves the number of likes and increment it by 1.
+    let count = Number(span.textContent.replace("likes", ""));
+    span.textContent = `${(count += 1)} likes`;
+  })
+);
 
 // items counter is returning 0
 // heart.forEach((icon) => icon.addEventListener('click', (event) => {
@@ -46,8 +49,8 @@ heartIcons.forEach((icon) => icon.addEventListener('click', (event) => {
 // }));
 
 // Add a click event listener to a parent element that contains the .commentBtn buttons
-document.body.addEventListener('click', (event) => {
-  if (event.target.classList.contains('commentBtn')) {
+document.body.addEventListener("click", (event) => {
+  if (event.target.classList.contains("commentBtn")) {
     const btn = event.target;
     //      alert('Button clicked: ' + btn.textContent); // Debugging alert
     const { id } = btn.parentElement.parentElement;
